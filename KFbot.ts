1d2eecd2af36c57592ecf8e9e6cb4cb95a96a42a
rohank36 <rohan.kanti@mail.mcgill.ca>
2024-05-01 16:27:32 -0400

    Added files

--- KFbot.ts
+++ KFbot.ts
@@ -12,12 +12,33 @@ bot.command("help", async (ctx) => {
 });
 
 //Start Command
-const startMenu = "<b>Start Menu</b>\n\nBlah blabh blagh balah asdf.";
+const startMenu = "<b>Start Menu</b>\n\nWelcome to KaizenFlo💪🌊🔥\n\nCongratulations on taking that fantastic step towards growth and learning off the mats. To help me better understand your journey and support you along the way, could you share some details about yourself? I'd love to know your name, age, profession, where you live, and the gym you train at? Put a /userinfo tag before entering your response. Here\'s an example:\n\n /userinfo I'm John Danaher, 127 years old, professor training out of New Wave Jiu Jitsu in Austin, Texas.";
 bot.command("start", async (ctx) => {
     await ctx.reply(startMenu, {
         parse_mode: "HTML",
     });
 })
+
+
+function processPreTrg(){
+    console.log('in pretrg');
+}
+
+function processPostTrg(){
+    console.log('in posttrg');
+}
+
+function processWeeklyGoals(){
+    console.log('in weeklygoals');
+}
+
+function processUserInfo(){
+    console.log('in userinfo');
+}
+
+function processBadMsg(){
+    console.log('bad message')
+}
 
 //message handler
 bot.on("message", async (ctx)=>{
@@ -28,9 +49,25 @@ bot.on("message", async (ctx)=>{
     );
 
     if(ctx.message.text){
+        const msg = ctx.message.text.toLowerCase();
+        let res;
+        if (/^\/pretrg\b/.test(msg)) {
+            res = processPreTrg();
+        } else if (/^\/posttrg\b/.test(msg)) {
+            res = processPostTrg();
+        } else if (/^\/weeklygoals\b/.test(msg)) {
+            res = processWeeklyGoals();
+        } else if (/^\/userinfo\b/.test(msg)) {
+            res = processUserInfo();
+        } else {
+           res = processBadMsg();
+        }   
+        
+        /*
         await ctx.reply(ctx.message.text.toUpperCase(), {
             entities: ctx.message.entities,
         });
+        */
     }
 
 })
